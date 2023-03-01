3a6851eb3f868aabd62c669c308d979bcc901bb5
Su Timurturkan <gtimurturkan@gmail.com>
2023-03-01 14:50:56 -0500

    update dashboard

--- script.js
+++ script.js
@@ -1,99 +1,200 @@
-//global variables
-var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9"
-var city = "atlanta"
-var currentConditions = "https://api.openweathermap.org/data/2.5/weather?appid="
-var fiveDay =
-  "https://api.openweathermap.org/data/2.5/forecast?4e5dbe7db2b5e9c8b47fa40b691443d5q={city name},{country code}"
-var uvIndex =
-  "https://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
-var searchedArr = JSON.parse(localStorage.getItem("searchedItems")) || [];
-
-
-//taking in user input, and passing the value into a variable
 $(document).ready(function() {
-  $("#search-input").on("click", function(event) {
-    var userInput = $("#city-search").val()
-    console.log(userInput)
-    getWeather(userInput)
+	// Event Click for Search Button
+	$("#search-button").on("click", function(event) {
+	  event.preventDefault();
+	  var city = $("#city").val();
+	  if (city == "") {
+		return;
+	  } else {
+		getCityWeather(city);
+		addToRecentSearches(city);
+	  }
+	});
   
-  })
-})
+	// Onclick listener to search list items
+	$("#recent-searches-list").on("click", "li.list-group-item", function() {
+	  var city = $(this).text();
+	  getCityWeather(city);
+	});
+  
+	// Hide Elements til item is searched
+	$("#city-info").hide();
+	$("#forecast").hide();
+  
+	// Load Recent Searches from Local Storage
+	getRecentSearches();
+  
+	
+	// City's Weather Info
+	function getCityWeather(city) {
+	  $("#city-info").show();
+  
+	  var api_key = "ef9704d691706b1a922e0f0e52268d4e";
+	  var baseURL = `https://api.openweathermap.org/data/2.5/weather?appid=${api_key}`;
+  
+	  city = city;
+	  var unit = "imperial";
+	  var newURL = baseURL + "&q=" + city + "&units=" + unit;
+  
+	  $.ajax({
+		url: newURL,
+		method: "GET"
+	  }).then(function(response) {
+		// City Name
+		$("#city-name").text(response.name);
+  
+		// Today's Date
+		$("#date-today").text(`(${moment().format("l")})`);
+  
+		// Weather Icon
+		$("#weather-icon").attr(
+		  "src",
+		  `https://openweathermap.org/img/wn/${response.weather[0].icon}.png`
+		);
+  
+		// Temperature in Fahrenheit
+		$("#temperature").text(response.main.temp + " F");
+  
+		// Humidity Percentage
+		$("#humidity").text(response.main.humidity + " %");
+  
+		// Wind Speed: MPH
+		$("#wind-speed").text(response.wind.speed + " MPH");
+  
+		// Get UV Index
+		var lon = response.coord.lon;
+		var lat = response.coord.lat;
+		getUVIndex(lon, lat);
+  
+		// 5 day forecast
+		var id = response.id;
+		getWeekForecast(id);
+	  });
+	}
+  
+	// UV Index
+	function getUVIndex(lon, lat) {
+	  var api_key = "ef9704d691706b1a922e0f0e52268d4e";
+	  var baseURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${api_key}`;
+  
+	  var newURL = baseURL + "&lat=" + lat + "&lon=" + lon;
+  
+	  $.ajax({
+		url: newURL,
+		method: "GET"
+	  }).then(function(response) {
+		var uv = response.value;
+  
+		if (uv <= 2.0) {
+		  // Favorable: 2 OR LESS
+		  $("#uv-index").text(uv);
+		  $("#uv-index").addClass("badge badge-success");
+		} else if (uv > 2.0 && uv <= 5.0) {
+		  // Medium: 2.1 TO 5
+		  $("#uv-index").text(uv);
+		  $("#uv-index").addClass("badge badge-warning");
+		} else if (uv > 5.0 && uv <= 10.0) {
+		  // HIGH: 5.1 to 10
+		  $("#uv-index").text(uv);
+		  $("#uv-index").addClass("badge badge-danger");
+		}
+	  });
+	}
+  
+	// Five Day forecast
+	function getWeekForecast(id) {
+	  $("#forecast").show();
+  
+	  var api_key = "ef9704d691706b1a922e0f0e52268d4e";
+	  var baseURL = `https://api.openweathermap.org/data/2.5/forecast?appid=${api_key}`;
+  
+	  var unit = "imperial";
+	  var newURL = baseURL + "&id=" + id + "&units=" + unit;
+  
+	  $.ajax({
+		url: newURL,
+		method: "GET"
+	  }).then(function(response) {
+		var cardHTML = "";
+  
+		// Loop for five day
+		for (var i = 1; i < response.list.length; i += 8) {
+		  // icon from response
+		  var weatherIcon = response.list[i].weather[0].icon;
+  
+		  //  Date to Month/Date/Year Format
+		  var dateStr = response.list[i].dt_txt;
+		  var dateStrArr = dateStr.split(" ");
+		  var date = dateStrArr[0];
+		  var dateArr = date.split("-");
+		  var newDate = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
+			//card for five day
+		  cardHTML += `
+			  <div class="card text-white bg-dark p-1 mr-3">
+				  <div class="card-header text-center font-weight-bold">${newDate}</div>
+				  <div class="card-body">
+				  <p class="card-text text-center">
+					  <img id="weather-icon" src="https://openweathermap.org/img/wn/${weatherIcon}.png"/>
+				  </p>
+				  <p class="card-text">
+					  Temp: ${response.list[i].main.temp} F
+				  </p>
+				  <p class="card-text">
+					  Humidity: ${response.list[i].main.humidity}%
+				  </p>
+				  </div>
+			</div>`;
+  
+		  $("#city-week-forecast").html(cardHTML);
+		}
+	  });
+	}
+  
+	// Add new city to Recent Searches list
+	var cities = [];
+  
+	function addToRecentSearches(city) {
+	  $("#recent-searches").show();
+  
+	  // Create Element
+	  var newCity = $("<li>");
+	  newCity.addClass("list-group-item");
+	  newCity.text(city);
+	  // Append to List
+	  $("#recent-searches-list").prepend(newCity);
+  
+	  var cityObj = {
+		city: city
+	  };
+  
+	  cities.push(cityObj);
+  
+	  // Save to localStorage
+	  localStorage.setItem("searches", JSON.stringify(cities));
+	}
+  
+	// Get Recent Searches from localStorage
+	function getRecentSearches() {
+	  var searches = JSON.parse(localStorage.getItem("searches"));
+	  if (searches != null) {
+		for (var i = 0; i < searches.length; i++) {
+		  // Create Element
+		  var newCity = $("<li>");
+		  newCity.addClass("list-group-item");
+		  newCity.text(searches[i].city);
+		  // Append to List
+		  $("#recent-searches-list").prepend(newCity);
+		}
+		$("#recent-searches").show();
+	  } else {
+		$("#recent-searches").hide();
+	  }
+	}
+  });
+  
 
-// userInput is passed into the getWeather function as arguement 'cityName'
-function getWeather(cityName) {
-  var apiCall = ""
-
-  if (cityName !== "") {
-    apiCall = currentConditions + apiKey + "&q=" + cityName
-    //return apiCall;
-  } else {
-    apiCall = currentConditions + apiKey + "&q=" + city
-    //return apiCall;
-  }
-
-  $.ajax({
-    url: apiCall,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response)
-    var feelslike = response.main.temp
-    feelslike = (feelslike - 273.15) * 1.8 + 32
-    feelslike = Math.floor(feelslike)
-    city = response.name
-    $("#current-weather").append("<div>" + feelslike + "</div>")
-    $("#current-weather").append("<div>" + city + "</div>")
-    fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-
-     $.ajax({
-      url: fiveDay,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response)
-
-      var averageTemp = 0
-      var previousdate = ""
-      var count = 0
-      var results = 0
-      previousdate = moment().format("MM/DD/YYYY")
-      for (let index = 0; index < response.list.length; index++) {
-        var currentDate = moment(response.list[index].dt, "X").format(
-          "MM/DD/YYYY"
-        )
-        var temp = response.list[index].main.temp
-        temp = (temp - 273.15) * 1.8 + 32
-        temp = Math.floor(temp)
-        console.log(currentDate)
-        console.log(temp)
-
-        if (previousdate === currentDate) {
-          averageTemp = averageTemp + temp
-          count++
-          previousdate = currentDate
-        } else {
-          results = averageTemp / count
-          results = Math.floor(results)
-          console.log("results:", results)
-          var card = $("<div class = 'card col-sm-2'>")
-
-          var div1 = $("<div class= 'card-header'>")
-          div1.append("Date" + '' + currentDate)
-          card.append(div1)
-
-          var div2 = $("<div class= 'card-body'>")
-          div2.append("Average Temperature:" + results)
-          card.append(div2)
-
-          $("#five-day").append(card)
-
-          count = 0
-          averageTemp = 0
-          previousdate = currentDate
-
-
-
-         
-        }
-      }
-    })
-  })
-}
+  //This clears the previous searches when user clicks on clear history button.
+$(".clear").on("click", function() {
+    localStorage.clear();
+    $("#recent-searches-list").empty();
+});
