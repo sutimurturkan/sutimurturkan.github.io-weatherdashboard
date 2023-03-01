8a4d93f71db992f2c2589cff47b8a7af5bb73905
Su Timurturkan <gtimurturkan@gmail.com>
2023-03-01 17:02:09 -0500

    update

--- script.js
+++ script.js
@@ -1,67 +1,105 @@
-const apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
-let lat = "";
-let lon = "";
-let city = "";
+// API key for OpenWeatherMap
+const apiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 
-const weatherDiv = document.getElementById("weather");
-const searchForm = document.querySelector("form");
-const searchInput = document.getElementById("city");
+// Query selectors for HTML elements
+const searchFormEl = document.querySelector('#search-form');
+const searchInputEl = document.querySelector('#search-input');
+const searchHistoryEl = document.querySelector('#search-history');
+const currentWeatherEl = document.querySelector('#current-weather');
+const forecastEl = document.querySelector('#forecast');
 
-searchForm.addEventListener("submit", (event) => {
+// Event listener for search form submission
+searchFormEl.addEventListener('submit', (event) => {
   event.preventDefault();
-  city = searchInput.value.trim();
-  localStorage.setItem("city", city);
-  getWeatherData(city);
+  const searchQuery = searchInputEl.value.trim();
+  if (searchQuery) {
+    // Call the OpenWeatherMap API to get the weather data
+    getWeatherData(searchQuery)
+      .then(data => {
+        // Update the UI with the weather data
+        updateSearchHistory(searchQuery);
+        updateCurrentWeather(data.currentWeather);
+        updateForecast(data.forecast);
+        saveToLocalStorage(searchQuery);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 });
 
-function getWeatherData(city) {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-  fetch(url)
+// Function to call the OpenWeatherMap API and get the weather data for a city
+function getWeatherData(cityName) {
+  // First, get the latitude and longitude of the city
+  return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
-      lat = data.city.coord.lat;
-      lon = data.city.coord.lon;
-      localStorage.setItem("lat", lat);
-      localStorage.setItem("lon", lon);
-      displayWeatherData(data);
-    })
-    .catch(error => console.log(error));
+      const { lat, lon } = data[0];
+      // Use the latitude and longitude to get the current weather and 5-day forecast
+      const currentWeatherPromise = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+        .then(response => response.json());
+      const forecastPromise = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+        .then(response => response.json());
+      return Promise.all([currentWeatherPromise, forecastPromise])
+        .then(data => {
+          return {
+            currentWeather: data[0],
+            forecast: data[1].list.filter(item => item.dt_txt.endsWith('12:00:00'))
+          };
+        });
+    });
 }
 
-function displayWeatherData(data) {
-  weatherDiv.innerHTML = "";
-  const forecasts = data.list.filter(item => item.dt_txt.includes("12:00:00"));
-  forecasts.forEach(forecast => {
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
-
-    const date = new Date(forecast.dt * 1000);
-    const options = { weekday: "short", month: "short", day: "numeric" };
-    const dateString = date.toLocaleDateString("en-US", options);
-
-    const iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-    const description = forecast.weather[0].description;
-
-    const temperature = Math.round(forecast.main.temp - 273.15);
-    const humidity = forecast.main.humidity;
-    const windSpeed = Math.round(forecast.wind.speed * 3.6);
-
-    cardDiv.innerHTML = `
-      <h2>${dateString}</h2>
-      <img src="${iconUrl}" alt="${description}">
-      <p>${description}</p>
-      <p>Temperature: ${temperature}°C</p>
-      <p>Humidity: ${humidity}%</p>
-      <p>Wind Speed: ${windSpeed} km/h</p>
-    `;
-
-    weatherDiv.appendChild(cardDiv);
+// Function to update the search history UI with a new search query
+function updateSearchHistory(searchQuery) {
+  const searchHistoryItemEl = document.createElement('li');
+  searchHistoryItemEl.textContent = searchQuery;
+  searchHistoryItemEl.addEventListener('click', () => {
+    searchInputEl.value = searchQuery;
+    searchFormEl.dispatchEvent(new Event('submit'));
   });
+  searchHistoryEl.appendChild(searchHistoryItemEl);
 }
 
-if (localStorage.getItem("city") && localStorage.getItem("lat") && localStorage.getItem("lon")) {
-  city = localStorage.getItem("city");
-  lat = localStorage.getItem("lat");
-  lon = localStorage.getItem("lon");
-  getWeatherData(city);
+// Function to update the current weather UI with the current weather data
+function updateCurrentWeather(currentWeatherData) {
+  currentWeatherEl.innerHTML = `
+    <h2>${currentWeatherData.name} (${new Date().toLocaleDateString()})</h2>
+    <img src="https://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}.png" alt="${currentWeatherData.weather[0].description}">
+    <p>Temperature: ${currentWeatherData.main.temp} &deg;C</p>
+    <p>Humidity: ${currentWeatherData.main.humidity}%</p>
+    <p>Wind Speed: ${currentWeatherData.wind.speed} m/s</p>
+  `;
 }
+
+// Function to update the forecast UI with the forecast data
+function updateForecast(forecastData) {
+  forecastEl.innerHTML = '';
+  forecastData.forEach(item => {
+  const forecastItemEl = document.createElement('div');
+  forecastItemEl.classList.add('forecast-item');
+  forecastItemEl.innerHTML = `<h3>${new Date(item.dt_txt).toLocaleDateString()}</h3> <img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png" alt="${item.weather[0].description}"> <p>Temperature: ${item.main.temp} &deg;C</p> <p>Humidity: ${item.main.humidity}%</p> <p>Wind Speed: ${item.wind.speed} m/s</p>` ;
+  forecastEl.appendChild(forecastItemEl);
+  });
+  }
+
+// Function to save the search history to localStorage
+function saveToLocalStorage(searchQuery) {
+  const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  if (!searchHistory.includes(searchQuery)) {
+  searchHistory.push(searchQuery);
+  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  }
+  }
+
+// Function to load the search history from localStorage
+function loadFromLocalStorage() {
+  const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+  searchHistory.forEach(searchQuery => {
+  updateSearchHistory(searchQuery);
+  });
+  }
+
+  // Load the search history from localStorage on page load
+loadFromLocalStorage();
+  
